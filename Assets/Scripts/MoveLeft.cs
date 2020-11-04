4cff6cdc3f306841acbaebc0a02c8212b57041e7
Gabeauch <gab.beauchemin27@gmail.com>
2020-11-04 16:41:53 -0500

    Initial commit
    Initial commit

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 1;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // VARIABLE VERS UNE INSTANCE DE LA CLASSE RigidBody
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier = 1;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // DETECTER SI LA TOUCHE ESPACE EST APPUYÉE
+        // ET EST-CE QUE LE PERSONNAGE EST DÉCLARÉ AU SOL?
+
+
+        if ( Input.GetKeyDown(KeyCode.Space) && isOnGround )
+        {
+            // SI OUI, IMPRIMER LE MESSAGE "JUMP"
+            Debug.Log("JUMP");
+            // AJOUTER UNE FORVE VERS LE HAUT 
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse );
+            // DÉCLARER QUE LE PERSONNAGE N'EST PLUS AU SOL
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }   
+    }
+
+    // LORSQUE LE PERSONNAGE RENTRE EN COLLISION AVEC LE SOL
+    // DÉCLARER QU'IL EST AU SOL
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            dirtParticle.Play();
+        }
+        else if (collision.gameObject.CompareTag("Obstacle")) {
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            isOnGround = false;
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
+       
+    }
+
+
+}
+
